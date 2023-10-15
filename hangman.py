8bb0e4323633e7e8fe3f3be740c4d489f066bb2e
Agb-ovo <agbroko12@gmail.com>
2023-10-15 20:57:40 +0100

    Current word print issue

--- hangman.py
+++ hangman.py
@@ -2,15 +2,18 @@ import random
 import string
 from words import words
 
+worded = ["the", "good", "thebad", "and" "the ugly"]
+
 def get_valid_word(words):
     word = random.choice(words) #randomly chooses something from the list
-    while '-' in word or ' ' in words:
+    while '-' in word or ' ' in word:
         word = random.choice(words)
-
+       
     return word
 
 def hangman():
     word = get_valid_word(words)
+    print(word)
     word_letters = set(word) #letters in the word
     alphabet = set(string.ascii_uppercase)
     used_letters = set()  # what the user has guessed
@@ -19,11 +22,22 @@ def hangman():
     while len(word_letters) > 0:
         # letters used
         # ' '.join(['a', 'b', 'cd']) --> 'a b cd'
-        print('You have used these letters:,' ' '.join(used_letters))
+        print('You have used these letters:', ' '.join(used_letters))
 
         # what current word is (ie W - R D)
-        word_list = [letter if letter in used_letters else '-' for letter in word]
-        print('Current word: ', ' '.join(word_list))
+        # word_list = [letter if letter in used_letters else '-' for letter in word]
+        word_list = []  # Initialize an empty list to store the characters
+
+        for letter in word:
+            if letter in used_letters:
+                word_list.append(letter)  # If the letter is in used_letters, add it to word_list
+            else:
+                word_list.append('-')  # If the letter is not in used_letters, add '-' to word_list
+
+        print('Current word: ', ' '.join(word_list), flush=True)
+        print(word_list)
+        print(word_letters)
+
         user_letter = input('Guess a letter: ').upper()
         if user_letter in alphabet - used_letters:
             used_letters.add(user_letter)
