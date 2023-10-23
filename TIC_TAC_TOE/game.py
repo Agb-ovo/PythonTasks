3860a5f0730619abc2850af5be5eaef7b3c65fab
Agb-ovo <agbroko12@gmail.com>
2023-10-23 17:51:44 +0100

    added game functions

--- TIC_TAC_TOE/game.py
+++ TIC_TAC_TOE/game.py
@@ -25,7 +25,53 @@ class TicTacToe:
             # ['x', 'x', 'o'] ------> {(o, 'x'), (1,'x'), (2, 'o')}
             #if spot == ' ':
                 #moves.append(i)
-        #return moves       
+        #return moves
 
+    def empty_squares(self):
+        return ' ' in self.board
 
+    def num_empty_squares(self):
+        return self.board.count(' ')   
 
+    def make_move(self, square, letter):  
+        if self.board[square] == ' ':
+            self.board[square] = letter
+            if self.winner(square, letter):
+                self.current_winner = letter
+            return True
+        return False
+
+def play(game, x_player, o_player, print_game=True):
+    # returns the winner of the game(the letter)! or None for a tie
+    if print_game:
+        game.print_board_nums()
+
+    letter = 'X' #starting letter   
+    # iterate while the game still has empty squares
+    # (we dont have to worry about winner because well return
+    # that which breaks the loop)
+    while game.empty_squares():
+        #get the move from the appropriate player
+        if letter == 'O':
+            square = o_player.get_move(game)
+        else:
+            square = x_player.get_move(game)
+        # lets define a function to make a move
+        if game.make_move(square, letter):
+            if print_game:
+                print(letter + f' makes a move to square {square}')
+                game.print_board()
+                print('')  #just empty line
+
+            if game.current_winner:
+                if print_game:
+                    print(letter + ' wins!')
+
+                        
+
+            #after we made our move, we need to alternate letters
+            letter = 'O' if letter == 'X' else 'X'
+            if letter == 'X':
+                letter = 'O'
+            else:
+                letter = 'X'    
